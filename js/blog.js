2eb22499b614e9d3b4cd382c9de506953603cfbf
Ankit Soni <akt_soni@yahoo.co.in>
2021-11-30 22:06:06 +0100

    aria label added for a tags

--- js/blog.js
+++ js/blog.js
@@ -7,13 +7,13 @@ async function getBlogs() {
     try {
         const response = await fetch(postsEmbedUrl);
         const results = await response.json();
-    
-        results.forEach(result=> {
-            creatHtml(result)  
-        })   
-    } 
+
+        results.forEach(result => {
+            creatHtml(result)
+        })
+    }
     catch (error) {
-        displayMsg( "","error-msg");
+        displayMsg("", "error-msg");
         console.log(error);
     }
 }
@@ -26,18 +26,18 @@ viewMoreBtn.addEventListener("click", getBlogs);
 
 
 //creates html inside async function
-function creatHtml (result) {
+function creatHtml(result) {
     const date = new Date(result.date).toDateString();
     const altText = result._embedded["wp:featuredmedia"][0].alt_text;
-  
-    blogsContainer.innerHTML += 
-        `<a href= "../blog-specific-page.html?id=${result.id}" class="article "> 
+
+    blogsContainer.innerHTML +=
+        `<a href= "../blog-specific-page.html?id=${result.id}" class="article " aria-label = "link to ${result.title.rendered}"> 
             <img src="${result.jetpack_featured_media_url}" alt="${altText}" />
             <div>
                 <p class="date">${date}</p>
                 <h2>${result.title.rendered}</h2>
                 <div class ="line-height"><p> ${result.content.rendered}</p></div>
-                <p class ="read-more">Read more </p>
+                <p class ="read-more" >Read more </p>
             </div>
         </a>`
 }
